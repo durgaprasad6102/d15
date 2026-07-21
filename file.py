d623cad05fd758426c510de36f27253bbd4e62f4
durgaprasad6102 <durgaprasad41930@gmail.com>
2026-07-21 19:08:49 +0530

    commit

---
+++ file.py
@@ -0,0 +1,22 @@
+cur_file = open("sample.txt","r")
+# print (cur_file.readline())
+
+
+
+# file = open("C:/room.txt.txt", "r")
+# print(file.readline())
+# file.close()
+
+# file = open("C:/Users/amman/OneDrive/Desktop/calculater/sampli.txt.txt", "r")
+# print(file.readline())
+
+
+# f=open("./sample.txt")
+# print(f.readline())
+# print(f.readline())
+
+# f=open("./nest/next1.txt")
+# print(f.readline())
+
+#file = open("sample.txt")
+# print(context.readlines())
