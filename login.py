e945614170312322bcf2ab8f9c55e7b1e6741743
durgaprasad6102 <durgaprasad41930@gmail.com>
2026-07-22 19:09:54 +0530

    Replace file.py with login.py

---
+++ login.py
@@ -0,0 +1,8 @@
+user_name="prasad"
+user_password="serect_123"
+
+def login(name password):
+    if user_name == name and user_password == password:
+        print("login succesfully")
+    else:
+        print("invaild")
